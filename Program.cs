b5728ecb4e13267436ff94102957aacb847228b8
r96922081 <r96922081@gmail.com>
2025-04-03 16:04:14 +0800

    .

--- Program.cs
+++ Program.cs
@@ -1,22 +1,43 @@
 ﻿using RegexNs;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 class Program
 {
+    static void Recognize(string pattern, string text)
+    {
+        bool accepted = Regex.Recognize(pattern, text);
+        Console.WriteLine("pattern = \"" + pattern + "\", text = \"" + text + "\", accepted = " + accepted);
+    }
+
+    static void Match(string pattern, string text)
+    {
+        List<string> allMatch = Regex.MatchAll(pattern, text);
+        Console.WriteLine("pattern = \"" + pattern + "\"");
+        Console.WriteLine("text = \"" + text + "\"");
+        Console.WriteLine("matched = \"" + string.Join("\", \"", allMatch) + "\"");
+    }
+
+
     static void Demo()
     {
-        string datePattern = "[1-9][0-9]{3}\\-[0-3][0-9]\\-[0-3][0-9]";
-        string dateInput1 = "2024-12-25";
-        string dateInput2 = "2024-99-99";
-        Trace.Assert(Regex.Recognize(datePattern, dateInput1) == true);
-        Trace.Assert(Regex.Recognize(datePattern, dateInput2) == false);
+        Recognize("((AB|BC)*|DE)", "ABBCAB");
+        Recognize("((AB|BC)*|DE)", "ABDE");
+        Console.WriteLine();
 
-        string emailPattern = "[a-zA-Z0-9\\.\\-]+@[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)+";
-        string emailInput = "Here are my emails: my-account@gmail.com, my-account@yahoo.com. Welcome to mail me";
-        List<string> allMatch = Regex.MatchAll(emailPattern, emailInput);
-        Trace.Assert(allMatch.Count == 2);
-        Trace.Assert(allMatch[0] == "my-account@gmail.com");
-        Trace.Assert(allMatch[1] == "my-account@yahoo.com");
+        Recognize("-?\\d+(\\.\\d+)?", "-123");
+        Recognize("-?\\d+(\\.\\d+)?", "-123.456");
+        Recognize("-?\\d+(\\.\\d+)?", "123.7A");
+        Console.WriteLine();
+
+        Recognize("[1-9][0-9]{3}\\-[0-3][0-9]\\-[0-3][0-9]", "2024-12-25");
+        Recognize("[1-9][0-9]{3}\\-[0-3][0-9]\\-[0-3][0-9]", "2024-99-99");
+        Console.WriteLine();
+
+        Match("[a-zA-Z0-9\\.\\-]+@[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)+", "Here are my emails: my-account@gmail.com, another12345@yahoo.com. Welcome to mail me");
+
+        Console.WriteLine();
+        Match("-?\\d+(\\.\\d+)?", "1.23 + 45.6 * 22 = 1004.43");
     }
 
     static void Ut()
@@ -26,7 +47,7 @@ class Program
 
     static void Main(String[] args)
     {
-        Ut();
+        //Ut();
         Demo();
     }
 }
